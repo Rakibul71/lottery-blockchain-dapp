87bcfe2fafc7e214a5d7b7b7f29afef9842541f2
Rakibul71 <rakibulislam202019@gmail.com>
2022-02-08 13:59:18 +0600

    complete design

--- pages/index.js
+++ pages/index.js
@@ -28,13 +28,65 @@ export default function Home() {
               <div className=" column is-two-thirds">
                 <section className="mt-5">
                   <p>Enter the lottery by sending 0.1 Ether</p>
-                  <button className="button is-link is-large is-large mt-3">
+                  <button className="button is-link is-large is-light mt-3">
                     Play Now
                   </button>
                 </section>
+
+                <section className="mt-6">
+                  <p>
+                    <b>Admin Only: </b> Pick Winner
+                  </p>
+                  <button className="button is-primary is-large is-light mt-3">
+                    Pick Winner
+                  </button>
+                </section>
               </div>
-              <div className=" column is-one-third">
-                <p>Lottery Info</p>
+
+              <div className={`${styles.lotteryInfo} column is-one-third`}>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Lottery History</h2>
+                        <div className="history-entry">
+                          <div>Lottery #1 winner</div>
+                          <a
+                            href="https://etherscan.io/address/0x978433520FfF6e433bB067945a1B31ebb7CB068C"
+                            target="_blank"
+                          >
+                            0x978433520FfF6e433bB067945a1B31ebb7CB068C
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Players (1)</h2>
+                        <a
+                          href="https://etherscan.io/address/0x978433520FfF6e433bB067945a1B31ebb7CB068C"
+                          target="_blank"
+                        >
+                          0x978433520FfF6e433bB067945a1B31ebb7CB068C
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </section>
+                <section className="mt-5">
+                  <div className="card">
+                    <div className="card-content">
+                      <div className="content">
+                        <h2>Pot</h2>
+                        <p>10 Ether</p>
+                      </div>
+                    </div>
+                  </div>
+                </section>
               </div>
             </div>
           </section>
